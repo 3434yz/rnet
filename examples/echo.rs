612521bf6182a61bfde8e6074117e693bf2686b0
belmont <w672317451@gmail.com>
2025-12-28 15:02:19 +0800

    lib

---
+++ examples/echo.rs
@@ -0,0 +1,54 @@
+use rnet::connection::Connection;
+use rnet::engine::{EngineBuilder, EngineHandler};
+use rnet::handler::{Action, Context, EventHandler};
+use rnet::options::Options;
+
+use bytes::BytesMut;
+
+use std::io::Write;
+
+#[derive(Clone, Copy)]
+struct MyHandler {
+    engine: Option<EngineHandler>,
+}
+
+impl EventHandler for MyHandler {
+    type Job = Vec<u8>;
+
+    fn on_open(&self, _conn: &mut Connection) -> Action<Self::Job> {
+        println!("New Connect");
+        Action::None
+    }
+
+    fn on_traffic(&self, conn: &mut Connection, cache: &mut BytesMut) -> Action<Self::Job> {
+        if let Some(datas) = conn.next(None, cache) {
+            let _ = conn.write(datas);
+        }
+        Action::None
+    }
+
+    fn on_close(&self, _ctx: &mut Context) -> Action<Self::Job> {
+        println!("Close Connect");
+        Action::None
+    }
+}
+
+fn main() {
+    let mut options = Options::builder()
+        .reuse_port(true)
+        .reuse_addr(true)
+        .multicore(true)
+        .build();
+
+    let addrs = vec!["tcp://127.0.0.1:9000"];
+    let net_socket_addrs = options.normalize(&addrs).unwrap();
+    let my_handler = MyHandler{
+        engine:None
+    };
+
+    let (mut engine, _handler) = EngineBuilder::builder()
+        .address(net_socket_addrs)
+        .build::<MyHandler>(options, my_handler);
+
+    engine.run().expect("run failed");
+}
